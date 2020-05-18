c3fc347b2e6f3d31dbc39627e111f6974c8cc4c3
Edward Beard <ejb207@cam.ac.uk>
2020-05-18 14:34:45 +0100

    Addition of specific resistances and logic to ignore resistances without units

--- tests/test_model.py
+++ tests/test_model.py
@@ -71,6 +71,8 @@ class TestModel(unittest.TestCase):
             'semiconductor': None,
             'active_area': None,
             'solar_simulator': None,
+            'specific_charge_transfer_resistance': None,
+            'specific_series_resistance': None,
             'electrolyte': None,
             'substrate': None,
             'charge_transfer_resistance': None,
@@ -84,6 +86,75 @@ class TestModel(unittest.TestCase):
         result = record.serialize(include_none=True)
         pprint(record)
         self.assertEqual(result, expected)
+
+    def test_model_initialized_resistance_property(self):
+
+        input = {'charge_transfer_resistance': {'ChargeTransferResistance': {'raw_value': '6.75 ',
+                                                                         'specifier': 'CE',
+                                                                             'value': [6.75]}},
+                              'ff': {'FillFactor': {'raw_value': '0.664',
+                                                    'specifier': 'FF',
+                                                    'value': [0.664]}},
+                              'jsc': {'ShortCircuitCurrentDensity': {'raw_units': '(mAcm–2)',
+                                                                     'raw_value': '15.49',
+                                                                     'specifier': 'Jsc',
+                                                                     'units': '(10^1.0) '
+                                                                              '* '
+                                                                              'Ampere^(1.0)  '
+                                                                              'Meter^(-2.0)',
+                                                                     'value': [15.49]}},
+                              'pce': {'PowerConversionEfficiency': {'raw_units': '(%)',
+                                                                    'raw_value': '7.78',
+                                                                    'specifier': 'PCE',
+                                                                    'units': 'Percent^(1.0)',
+                                                                    'value': [7.78]}},
+                              'voc': {'OpenCircuitVoltage': {'raw_units': '(mV)',
+                                                             'raw_value': '756',
+                                                             'specifier': 'Voc',
+                                                             'units': '(10^-3.0) '
+                                                                      '* '
+                                                                      'Volt^(1.0)',
+                                                             'value': [756.0]}}}
+
+        expected = {
+            'voc': {'OpenCircuitVoltage': {'raw_units': '(mV)', 'raw_value': '756', 'specifier': 'Voc',
+                                       'units': '(10^-3.0) * Volt^(1.0)', 'value': [756.0]}},
+            'pce': {'PowerConversionEfficiency': {'raw_units': '(%)', 'raw_value': '7.78', 'specifier': 'PCE',
+                                              'units': 'Percent^(1.0)', 'value': [7.78]}},
+            'ff': {'FillFactor': {'raw_value': '0.664', 'specifier': 'FF', 'value': [0.664]}},
+            'jsc': {'ShortCircuitCurrentDensity': {'raw_units': '(mAcm–2)',
+                                                   'raw_value': '15.49',
+                                                   'specifier': 'Jsc',
+                                                   'units': '(10^1.0) '
+                                                            '* '
+                                                            'Ampere^(1.0)  '
+                                                            'Meter^(-2.0)',
+                                                   'value': [15.49]}},
+            'dye': None,
+            'ref': None,
+            'isc': None,
+            'redox_couple': None,
+            'dye_loading': None,
+            'counter_electrode': None,
+            'semiconductor': None,
+            'active_area': None,
+            'solar_simulator': None,
+            'specific_charge_transfer_resistance': None,
+            'specific_series_resistance': None,
+            'electrolyte': None,
+            'substrate': None,
+            'charge_transfer_resistance': None,
+            'series_resistance': None,
+            'exposure_time': None,
+            'table_row_categories': None,
+            'calculated_properties': None
+        }
+
+        record = PhotovoltaicRecord(input)
+        result = record.serialize(include_none=True)
+        pprint(record)
+        self.assertEqual(result, expected)
+
 
     def test_substitute_abbreviations(self):
         input = {'counter_electrode': {'CounterElectrode': {'raw_value': 'Ni-Co-Se',
